bf018ad98478e430f83c1e15950b11eb2b0fc901
zul1996 <mzulnewbie@gmail.com>
2022-10-11 16:42:48 +0700

    Struktur folder project

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return <div className="flex justify-centert">Navigation</div>;
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,29 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">test</h1>;
+  return (
+    <div>
+      <head>
+        <title>Petshub</title>
+      </head>
+
+      {/* Header */}
+      <Header />
+
+      {/* Hero */}
+
+      {/* nyan nyan product */}
+
+      {/* best sellers */}
+
+      {/* satisfaction guarantee */}
+
+      {/* Sign up + save newslatter */}
+
+      {/* footer */}
+    </div>
+  );
 };
 
 export default Home;
