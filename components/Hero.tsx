a4cb8e81f701f4b7315aac2b1cc40c82f02966f0
Makmur Emha <makmur.emha@gmail.com>
2022-10-12 00:57:05 +0800

    fix : improve component hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,22 +5,20 @@ type Props = {};
     
 function Hero({}: Props) {
   return (
-    <div className="box-border ">
-      <div className="grid grid-cols-2 px-20 ">
-        <div>
+    <div className="container relative ">
+      <div className="grid grid-cols-2 m-10 align-middle">
+        <div className="p-10 text-left">
           <h1 className="text-4xl font-bold">BEST PRODUCT</h1>
           <h1 className="text-4xl font-bold">FOR YOUR PETS</h1>
-          <article className="prose lg:prose-xl">
-            <p>
-              Looking for the best food, accessories and more for your pet? Look no further than PetsHub! We offer everything you need to keep your pet happy and healthy, all while providing amazing customer service. We know that your pet is a member of the family, and we treat them as such!
-            </p>
-          </article>
+          <p className="text-lg mt-5">
+            Looking for the best food, accessories and more for your pet? Look no further than PetsHub! We offer everything you need to keep your pet happy and healthy, all while providing amazing customer service. We know that your pet is a member of the family, and we treat them as such!
+          </p>
         </div>
-        <div>
-          <Image src="/hero-object.png" width={300} height={230} />
+        <div className="ml-10 text-center">
+          <Image src="/hero-object.png" width={400} height={280} />
         </div>
       </div>
-    </div>
+      </div>
   );
 }
 
