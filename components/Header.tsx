95cb882b589a8acaa1677510d85455e5dd5dd2d8
zul1996 <mzulnewbie@gmail.com>
2022-10-11 16:45:18 +0700

    fix error className tailwind

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div className="flex justify-centert">Navigation</div>;
+  return <div className=" flex justify-center">Navigation</div>;
 };
 
 export default Header;
