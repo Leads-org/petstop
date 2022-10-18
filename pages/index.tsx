4246d508797644dc6db99af3578712ff3d8e80eb
Makmur Emha <makmur.emha@gmail.com>
2022-10-18 17:05:10 +0800

    fix : remove import unused

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,9 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Category from "../components/Category";
 import BestSeller from "../components/BestSeller";
-import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Satisfaction from "../components/Satisfaction";
 import Newsletter from "../components/Newsletter";
-import Footer from "../components/Footer";
 import Layout from "../components/Layout";
 
 const Home: NextPage = () => {
